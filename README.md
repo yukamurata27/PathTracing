4b1487081534b1bf7c3e2d02685d52129ee1c832
yukamurata27 <yukamurata27@gmail.com>
2019-11-21 23:55:16 -0800

    Setup output file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Path Tracing in One Weekend
+
+## Resources
+1) [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 /* C++ standard libraries */
 #include <iostream> // cout
+#include <fstream>  // file i/o
 
 /* Other headers */
 
@@ -9,7 +10,7 @@ using namespace std;
  * main
  *
  * Under src directory...
- * Compile: g++ -std=c++11 main.cpp loader.cpp -o main
+ * Compile: g++ -std=c++11 main.cpp -o main
  * Run:     ./main
  *
 */
@@ -22,6 +23,28 @@ int main(int argc, char * argv[])
 	//	return 0;
 	//}
 
+	ofstream outfile;
+	int nx = 200;
+	int ny = 100;
+
+	outfile.open ("output.ppm");
+	outfile << "P3\n" << nx << " " << ny << "\n255\n";
+
+	for (int j = ny-1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+			int ir = int(255.99 * r);
+			int ig = int(255.99 * g);
+			int ib = int(255.99 * b);
+			outfile << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+
+	outfile.close();
+
 	cout << "Path Tracer Completed!" << endl;
+	return 0;
 }
 
