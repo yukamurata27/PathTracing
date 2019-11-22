91dd348bc0bc90825ea9a98930ea146492583101
yukamurata27 <yukamurata27@gmail.com>
2019-11-22 02:46:57 -0800

    Add ray data type

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,12 @@
 #include <fstream>  // file i/o
 
 /* Other headers */
-#include "../include/vec3.h"
+// Include a header once once within a project!
+#include "../include/ray.h"
 
 using namespace std;
+
+vec3 color(const ray& r);
 
 /*
  * main
@@ -31,12 +34,23 @@ int main(int argc, char * argv[])
 	outfile.open ("output.ppm");
 	outfile << "P3\n" << nx << " " << ny << "\n255\n";
 
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0); // step interval
+	vec3 vertical(0.0, 2.0, 0.0);   // step interval
+	vec3 origin(0.0, 0.0, 0.0);
+
+	// Send a ray out of eye (0, 0, 0) from BL to UR corner
 	for (int j = ny-1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
-				int ir = int(255.99	* col[0]);
-				int ig = int(255.99	* col[1]);
-				int ib = int(255.99	* col[2]);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
+			ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+			vec3 col = color(r);
+
+			int ir = int(255.99	* col[0]);
+			int ig = int(255.99	* col[1]);
+			int ib = int(255.99	* col[2]);
+
 			outfile << ir << " " << ig << " " << ib << "\n";
 		}
 	}
@@ -45,5 +59,17 @@ int main(int argc, char * argv[])
 
 	cout << "Path Tracer Completed!" << endl;
 	return 0;
+}
+
+vec3 color(const ray& r) {
+	// Sky color is a linear interpolation b/w while & blue
+
+	vec3 unit_direction = unit_vector(r.direction());
+
+	// t is a mapped y from [-1, 1] to [0, 1]
+	// more accurately, t = 0.5 * (sqrt(2)*unit_direction.y() + 1.0)
+	float t = 0.5 * (unit_direction.y() + 1.0);
+
+	return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
