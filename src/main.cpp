44aeb359fcf2b94b4ddb17daa1ca121bfa7acf5e
yukamurata27 <yukamurata27@gmail.com>
2019-11-21 23:36:06 -0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+/* C++ standard libraries */
+#include <iostream> // cout
+
+/* Other headers */
+
+using namespace std;
+
+/*
+ * main
+ *
+ * Under src directory...
+ * Compile: g++ -std=c++11 main.cpp loader.cpp -o main
+ * Run:     ./main
+ *
+*/
+int main(int argc, char * argv[])
+{
+	cout << "Hello World!" << endl;
+
+	//if ( argc == 1 ) {
+	//	cout << "Expected: ./main obj_file_name" << endl;
+	//	return 0;
+	//}
+
+	cout << "Path Tracer Completed!" << endl;
+}
+
