b3fdf504318777ded73381d84960fda02e25992b
yukamurata27 <yukamurata27@gmail.com>
2019-11-23 04:31:37 -0800

    Positionable camera

--- include/camera.h
+++ include/camera.h
@@ -5,11 +5,21 @@
 
 class camera {
 	public:
-		camera() {
-			lower_left_corner = vec3(-2.0, -1.0, -1.0);
-			horizontal = vec3(4.0, 0.0, 0.0);
-			vertical = vec3(0.0, 2.0, 0.0);
-			origin = vec3(0.0, 0.0, 0.0);
+		// Still stick to z=-1 plane
+		camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) {
+			// vfov is top to bottom in degrees
+			float theta = vfov * M_PI / 180;
+			float half_height = tan(theta / 2);
+			float half_width = aspect * half_height;
+
+			vec3 u, v, w;
+			origin = lookfrom;
+			w = unit_vector(lookfrom - lookat); // depth
+            u = unit_vector(cross(vup, w)); // x direction
+            v = cross(w, u); // y direction (upvector)
+			lower_left_corner = origin - half_width*u - half_height*v - w;
+			horizontal = 2 * half_width * u;
+			vertical = 2 * half_height * v;
 		}
 
 		ray get_ray(float u, float v) {
--- src/main.cpp
+++ src/main.cpp
@@ -147,7 +147,7 @@ int main(int argc, char * argv[]) {
 	list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
 	hittable *world = new hittable_list(list, 5);
 
-	camera cam;
+	camera cam(vec3(-2,2,1), vec3(0,0,-1), vec3(0,1,0), 90, float(nx)/float(ny));
 
 	// Send a ray out of eye (0, 0, 0) from BL to UR corner
 	for (int j = ny-1; j >= 0; j--) {
